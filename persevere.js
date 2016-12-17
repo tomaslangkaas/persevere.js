1a1f051b52ccc979706b091bceabcb5e185a3155
tomaslangkaas <tomaslangkaas@users.noreply.github.com>
2016-12-17 21:06:50 +0100

    Update persevere.js

--- persevere.js
+++ persevere.js
@@ -74,7 +74,7 @@ var persevere = (function() {
         onFail && onFail();
       }
     },
-    'empty': function(onFail) {
+    'clear': function(onFail) {
       try {
         localStorage['clear']();
       } catch (e) {
